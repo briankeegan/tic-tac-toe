5ebd4366649449cf81538e620ca0db932576f74b
briankeegan <brian.morris.keegan@gmail.com>
2017-12-21 10:58:53 -0500

    Fix fundamental error in createBoard
    createBoard now returns an array(9) with empty strings

--- assets/scripts/tttlogic.js
+++ assets/scripts/tttlogic.js
@@ -1,10 +1,11 @@
 'use strict'
 
 const createBoard = function (input) {
-  return input || ['', '', '', '', '', '', '', '', '', '', '', '']
+  return input || ['', '', '', '', '', '', '', '', '']
 }
 
 const checkForWinner = function (board) {
+
   // If all moves have been made with no winner, its a draw
   if (board.every(move => move !== '')) {
     return 'Draw'
