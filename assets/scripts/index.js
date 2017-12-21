3895131ec5f30ca9264a5fb82bdf9ff50ef8d800
briankeegan <brian.morris.keegan@gmail.com>
2017-12-21 13:25:03 -0500

    Update #sign-up form
    Also I removed the logs in index.js, as testing worked

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,17 +9,7 @@ $(() => {
 })
 
 $(() => {
-  console.log(tttlogic.makeMove(0))
-  console.log(tttlogic.makeMove(4))
-  console.log(tttlogic.makeMove(1))
-  console.log(tttlogic.makeMove(7))
-  console.log(tttlogic.makeMove(2))
-  console.log(tttlogic.makeMove(0))
-  console.log(tttlogic.makeMove(6))
-  console.log(tttlogic.makeMove(7))
-  console.log(tttlogic.makeMove(8))
-  // console.log(tttlogic.makeMove(8))
-  console.log(tttlogic.board)
+
 })
 
 // use require with a reference to bundle the file and use it in this file
