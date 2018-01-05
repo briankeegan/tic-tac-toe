a33466682aa024de7b0f89926a29914e11e7ab22
briankeegan <brian.morris.keegan@gmail.com>
2018-01-05 15:13:55 -0500

    Revert back to original api
    -   I will now attempt to fix my api
    -   Roll back to last version

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -4,7 +4,7 @@ const config = {
   apiOrigins: {
     // development: 'http://tic-tac-toe.wdibos.com',
     development: 'https://aqueous-atoll-85096.herokuapp.com',
-    production: 'https://peaceful-wildwood-13431.herokuapp.com'
+    production: 'https://aqueous-atoll-85096.herokuapp.com'
   }
 }
 
